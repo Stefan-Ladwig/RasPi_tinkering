6c0e3f71a4b5070e091535bf92104551734da982
Stefan Ladwig <ladwig.stefan@gmx.de>
2021-11-24 02:32:28 +0100

    added blink animation + input buffer and did some bug fixes

--- snake_dotmatrix.py
+++ snake_dotmatrix.py
@@ -5,50 +5,39 @@ import numpy as np
 import time
 import random
 
-exit = 'PS'
-x_dir = 'DPAD-X'
-y_dir = 'DPAD-Y'
-
 controller = Gamepad.PS4()
+running = 1
+blink = 0.3
+next_move = 1
+moved = 0
+buffer = [0, (0, 0)]
+
 def new_game():
-    global mat
-    global pos_tuple
+    global mat, pos_tuple, speed, snake, field, food, ingame, direction
+    
     mat = np.full((8,8), 1)
     pos_tuple = [(i, j) for i in range(8) for j in range(8)]
-
-    global blink
-    global direction
-    global speed
-    global snake
-    global field
-    global food
-    global running
-    global ingame
-
-    blink = 0.3
     start = (0, 0)
-    direction = (1, 0)
-    speed = 0.5
+    direction = (1, 1)
+    speed = 0.3
     snake = [start]
     mat[snake[-1]] = 0
     field = pos_tuple
     field.remove(start)
     food = random.choice(field)
     mat[food] = 0
-    running = 1
     ingame = 0
 
 def move(direction):
-    global snake
-    global mat
-    global field
-    global food
-    global speed
+    global snake, mat, field, food, speed
+    
     head = snake[-1]
     snake.append((head[0] + direction[0],
                   head[1] + direction[1]))
     sh = snake[-1]
     if sh[0] not in range(8) or sh[1] not in range(8) or sh in snake[:-1]:
+        if head != food:
+            mat[food] = 1
         field_blink()
         new_game()
         return
@@ -59,23 +48,20 @@ def move(direction):
         mat[tail] = 1
         field.append(tail)
     else:
-        speed *= 0.95
+        speed *= 0.97
         mat[food] = 0
         food = random.choice(field)
         mat[food] = 0
 
 def field_blink():
     global mat
-    global food
-    global snake
-    if snake[-1] != food:
-        mat[food] = 1
+    
     time.sleep(0.1)
     for i in range(3):
         t = time.perf_counter()
         while time.perf_counter() - t < 0.2:
             sr.draw_frame(mat)
-        time.sleep(0.2)
+        time.sleep(0.1)
 
 # Wait for a connection
 if not Gamepad.available():
@@ -86,28 +72,34 @@ gamepad = Gamepad.PS4()
 print('Gamepad connected')
 
 # define callback functions
-def y_turn(plusminus):
-    global direction
-    global ingame
+def turn(axis_name, plusminus):
+    global direction, ingame, next_move, buffer, moved
+    
     if not ingame:
         ingame = 1
-    if direction[1] and plusminus:
-        direction = (int(plusminus), 0)
-                 
-def x_turn(plusminus):
-    global direction
-    global ingame
-    if not ingame:
-        ingame = 1
-    if direction[0] and plusminus:
-        direction = (0, int(plusminus))
-
+    if plusminus:
+        if next_move:
+            if axis_name[-1] == 'Y' and direction[1]:
+                direction = (int(plusminus), 0)
+            elif axis_name[-1] == 'X' and direction[0]:
+                direction = (0, int(plusminus))
+            next_move = 0
+            moved = 1
+        else:
+            buffer[0] = 1
+            if axis_name[-1] == 'Y':
+                buffer[1] = (int(plusminus), 0)
+            else:
+                buffer[1] = (0, int(plusminus))
+            
 def pause():
     global ingame
-    ingame = 0
+    
+    ingame = 1 - ingame
 
 def end_of_game():
     global running
+    
     if not ingame:
         running = 0
     else:
@@ -117,8 +109,8 @@ def end_of_game():
 controller.startBackgroundUpdates()
 
 # Register callback functions
-controller.addAxisMovedHandler('DPAD-X', x_turn)
-controller.addAxisMovedHandler('DPAD-Y', y_turn)
+controller.addAxisMovedHandler('DPAD-X', turn)
+controller.addAxisMovedHandler('DPAD-Y', turn)
 controller.addButtonPressedHandler('PS', end_of_game)
 controller.addButtonPressedHandler('CROSS', pause)
 
@@ -135,15 +127,20 @@ try:
         sr.draw_frame(mat)
         
         if ingame:
-            
-            if time.perf_counter() - t_frame > speed:
-                move(direction)
-                t_frame = time.perf_counter()
                 
             if time.perf_counter() - t_food > blink:
-                if not food in snake:
+                if food not in snake:    
                     mat[food] = 1 - mat[food]
                     t_food = time.perf_counter()
+            
+            if time.perf_counter() - t_frame > speed:
+                if buffer[0] and not moved:
+                    direction = buffer[1]
+                    buffer[0] = 0
+                move(direction)
+                next_move = 1
+                moved = 0
+                t_frame = time.perf_counter()
                  
 finally:
     # Ensure the background thread is always terminated when we are done
